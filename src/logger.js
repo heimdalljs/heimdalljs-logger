1727cb8e1dd8cb9fd4141ebc5fa43c26d77591b8
Stefan Penner <stefan.penner@gmail.com>
2016-08-01 10:26:54 -0700

    Update logger.js (#1)
    * Update logger.js

--- src/logger.js
+++ src/logger.js
@@ -40,14 +40,12 @@ class Logger {
   }
 }
 
-function noop() {}
-
 export let NULL_LOGGER = {
-  trace: noop,
-  debug: noop,
-  info: noop,
-  warn: noop,
-  error: noop,
+  trace() { },
+  debug() { },
+  info() { },
+  warn() { },
+  error() { }
 };
 
 export default Logger;
